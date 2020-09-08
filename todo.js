398dbc9d7509076d06b66e8e394fa29456194592
Alexandra Hamner <alexandra.m.hamner@gmail.com>
2020-09-07 22:29:08 -0500

    feat: added the render todo function.

--- todo.js
+++ todo.js
@@ -4,7 +4,7 @@
 
 (function () {
 
-// This holds todo list items.
+// This holds todoList items.
 	let todoItems = [];
 
 
@@ -18,7 +18,7 @@
 		};
 
 		todoItems.push(todo);
-		console.log(todoItems);
+		renderTodo(todo);
 
 	}
 
@@ -40,5 +40,24 @@
 			input.focus();
 		}
 	});
+	
+//	This renders the todoList.
+	function renderTodo(todo) {
+		const list = document.querySelector(".js-todo-list");
+		const isChecked = todo.checked ? 'done': '';
+		const node = document.createElement("li");
 
+		node.setAttribute('class', 'todo-item ${isChecked}');
+		node.setAttribute('data-key', todo.id);
+		node.innerHTML = `
+		<input id="${todo.id}" type="checkbox"/>
+		<label for="${todo.id}" class="tick js-tick"></label>
+		<span>${todo.text}</span>
+		<button class="delete todo js-delete-todo">
+		<svg><use href="#delete-icon"></use></svg>
+		</button>
+		`;
+
+		list.append(node);
+	}
 }) ();
